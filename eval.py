f5f9a2e0bba3c371df6c5334ce5dace1b977d46b
叶芃 <isdkfj@qq.com>
2022-07-10 18:34:20 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -39,17 +39,17 @@ def eval(net, data, bf, use_gpu):
         test_acc /= len(test_dataset)
     A = np.concatenate(A, axis=0)
     X = np.concatenate(X, axis=0)
-    #sol, val = leverage_score_solve(A, 20, net.d1 + 1)
+    sol, val = leverage_score_solve(A, 20, net.d1 + 1)
     '''cov = np.dot(A.T, A)
     for bid in bf:
         real_x = np.linalg.solve(cov, np.dot(A.T, X[:, bid].reshape(-1, 1)))
         print('error of feature no.{}:'.format(bid), np.sum((X[:, bid].reshape(-1, 1) - np.dot(A, real_x.reshape(A.shape[1], 1))) ** 2))
     print('error of solution:', val)'''
-    #rec = np.dot(A, sol.reshape(A.shape[1], 1))
-    #print('attack acc w.r.t. fake label:', np.sum(np.isclose(X[:, -1].reshape(-1, 1), rec > 0.5)) / X.shape[0])
+    rec = np.dot(A, sol.reshape(A.shape[1], 1))
+    print('attack acc w.r.t. fake label:', np.sum(np.isclose(X[:, -1].reshape(-1, 1), rec > 0.5)) / X.shape[0])
     idx, best_acc = 0, 0
-    #for i in range(net.d1):
-    #    acc = np.sum(np.isclose(X[:, i].reshape(-1, 1), rec > 0.5)) / X.shape[0]
-    #    if acc > best_acc:
-    #        idx, best_acc = i, acc
+    for i in range(net.d1):
+        acc = np.sum(np.isclose(X[:, i].reshape(-1, 1), rec > 0.5)) / X.shape[0]
+        if acc > best_acc:
+            idx, best_acc = i, acc
     return train_acc, test_acc, best_acc, idx
