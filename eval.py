fd739c406e6958a28cbe600e796e69ca11e2f767
叶芃 <isdkfj@qq.com>
2022-07-11 10:13:22 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -40,11 +40,11 @@ def eval(net, data, bf, use_gpu):
     A = np.concatenate(A, axis=0)
     X = np.concatenate(X, axis=0)
     sol, val = leverage_score_solve(A, 20, net.d1 + 1)
-    '''cov = np.dot(A.T, A)
+    cov = np.dot(A.T, A)
     for bid in bf:
         real_x = np.linalg.solve(cov, np.dot(A.T, X[:, bid].reshape(-1, 1)))
         print('error of feature no.{}:'.format(bid), np.sum((X[:, bid].reshape(-1, 1) - np.dot(A, real_x.reshape(A.shape[1], 1))) ** 2))
-    print('error of solution:', val)'''
+    print('error of solution:', val)
     rec = np.dot(A, sol.reshape(A.shape[1], 1))
     print('attack acc w.r.t. fake label:', np.sum(np.isclose(X[:, -1].reshape(-1, 1), rec > 0.5)) / X.shape[0])
     idx, best_acc = 0, 0
