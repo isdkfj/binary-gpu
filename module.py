34e3e499d84992d3a7f2c4959066749436c559de
叶芃 <isdkfj@qq.com>
2022-07-13 16:13:57 +0800

    Update module.py

--- module.py
+++ module.py
@@ -9,8 +9,8 @@ class Net(nn.Module):
         # d1 for passive party, d2 for active party
         self.d1 = d1
         self.d2 = d2
-        self.input1 = nn.Linear(d1 - 2, hidden[0], bias=False)
-        self.input1_sub = nn.Linear(d1, d1 - 2, bias=False)
+        self.input1 = nn.Linear(d1, hidden[0], bias=False)
+        self.input1_sub = nn.Linear(d1, d1 - 1, bias=False)
         self.input2 = nn.Linear(d2, hidden[0], bias=True)
         hidden_layers = []
         for i in range(len(hidden) - 1):
@@ -22,7 +22,7 @@ class Net(nn.Module):
         self.defense = defense
 
     def forward(self, x):
-        if True or isinstance(self.defense, Defense):
+        if False:
             x1 = self.input1_sub(x[:, :self.d1])
             #x1 = self.input1(torch.cat((x1, x[:, -1].reshape(-1, 1)), axis=1))
             #x1 += self.defense(x1.detach(), x.detach(), self.input1.weight.detach())
@@ -30,7 +30,6 @@ class Net(nn.Module):
         else:
             x1 = self.input1(x[:, :self.d1])
             x1 += self.defense(x1.detach(), x.detach(), self.input1.weight.detach())
-            #print('gauss')
         x1 = self.inter(x1)
         x2 = self.input2(x[:, self.d1: self.d1 + self.d2])
         x = x1 + x2
